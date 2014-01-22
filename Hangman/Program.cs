483e4bc8e59b73c60b3bdc326180f521fa673250
Havondor <Havondor@gmail.com>
2014-01-21 21:57:02 -0600

    Initial Work
    Began the word class, Made a basic UI

---
+++ Hangman/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Words wd = new Words();
+            Console.WriteLine("The Current Word is {0}", wd.HiddenWord());
+            Console.WriteLine("Enter a letter to try: ");
+
+            int x = 0;
+
+            while (1 == 1)
+            {
+                string Input = Console.ReadLine();
+                int Num;
+                if (string.IsNullOrEmpty(Input) || int.TryParse(Input, out Num))
+                {
+                    Console.WriteLine("Please Enter a Valid Letter Only");
+                }
+                else if (Input.Length > 1)
+                {
+                    Console.WriteLine("Please only Enter on letter at a time");
+                }
+                else
+                {
+                    char c = Input[0];
+                    wd.TryLetter(c);
+                    Console.WriteLine();
+                    Console.WriteLine("The Current Word is {0}", wd.HideWord);
+                    Console.WriteLine();
+                    Console.WriteLine("Enter a letter to try: ");
+                }
+
+                x += 1;
+                if (x == 2) 
+                {
+                    break;
+                }
+            }
+            
+
+        }
+    }
+}
---
+++ Hangman/Words.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    public class Words
+    {
+        private string CurrWord;
+        public string HideWord;
+
+        public Words() 
+        {
+            SelectWord();
+        }
+
+        private void SelectWord()
+        {
+            CurrWord = "hammer";
+        }
+
+        public string HiddenWord()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 1; i <= CurrWord.Length; i++)
+            {
+                sb.Append("_ ");
+            }
+
+            sb.Remove((sb.Length - 1), 1);
+            HideWord = sb.ToString();
+            return sb.ToString();
+        }
+
+        public void TryLetter(char letter)
+        {
+            int LetterIndex = CurrWord.IndexOf(letter);
+
+            if (LetterIndex != -1)
+            {
+                ShowLetter(letter);
+                Console.WriteLine("{0} is a correct letter!", letter);
+            }
+            else
+            {
+                Console.WriteLine("{0} is an incorrect letter!", letter);
+            }
+            
+        }
+
+        private void ShowLetter(char letter)
+        {
+            StringBuilder sb = new StringBuilder();
+            string ShrunkHideWord = HideWord.Replace(" ", "");
+
+            for (int i = 0; i < CurrWord.Length; i++)
+            {
+                char c = CurrWord[i];
+                char s = ShrunkHideWord[i];
+
+                if (c == letter)
+                {
+                    sb.Append(c);
+                }
+                else
+                {
+                    sb.Append(s);
+                }
+
+            }
+            
+            StringBuilder sb2 = new StringBuilder();
+
+            foreach (char c in sb.ToString())
+            {
+                sb2.Append(c).Append(" ");
+            }
+
+            HideWord = sb2.ToString().TrimEnd();
+        }
+
+    }
+}
